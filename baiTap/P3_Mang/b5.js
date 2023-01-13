3da93cbeb07b325ce0559a8a3cedbd41e50b6cdc
miraiMoon <miraimoon13@gmail.com>
2023-01-13 18:35:42 +0700

    v3

---
+++ baiTap/P3_Mang/b5.js
@@ -0,0 +1,4 @@
+console.log(`Viết chương trình cho phép người dùng nhập vào 5 mã số, lưu 5 mã số này vào một mảng.
+Chương trình sẽ kiểm tra xem trong 5 mã số này có mã số nào sai định dạng hay không (định dạng là “00yLxxxx” với y là số nguyên từ 2 - 5, x là số nguyên từ 0-9).
+Nếu có bất kỳ mã số nào sai định dạng thì chương trình in ra “Sai rồi” rồi kết thúc chương trình, ngược lại thì in ra “Đúng rồi”. (Sử dụng biểu thức chính quy để ràng buộc định dạng)
+`)
---
+++ baiTap/P4_Chuoi/b4.js
@@ -0,0 +1,21 @@
+console.log(`Viết chương trình cho phép nhập vào một chuỗi
+Kiểm tra xem chuỗi này ký tự “a” xuất hiện bao nhiêu lần
+In ra số lần đó.
+`);
+
+// let chuoi = prompt('Nhập vào một chuỗi: ');
+// console.log(`Chuỗi vừa nhập là: ${chuoi}`);
+
+// let mangKiTu = chuoi.split('');
+// let count = 0;
+
+// for(let i =0; i<mangKiTu.length; i++){
+//     if(mangKiTu == 'a'){
+//         count++;
+//     }
+//     else{
+//         console.log(`Không chứa ký tự 'a'`);
+//     }
+// }
+
+// console.log(count);
